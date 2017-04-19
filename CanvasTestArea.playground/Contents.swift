0191c4e0db756fb010c7feb92597a3c47981225e
Jeffrey Goldsmith <jgoldsmith@rsgc.on.ca>
2017-04-19 09:15:24 -0400

    Created simple drawing method to create a new word based on an axiom and rule and subsequently draw that word

--- CanvasTestArea.playground/Contents.swift
+++ CanvasTestArea.playground/Contents.swift
@@ -18,15 +18,56 @@ import Cocoa
 import PlaygroundSupport
 
 // Create a new canvas
-let canvas = Canvas(width: 300, height: 500)
+let canvas = Canvas(width: 500, height: 500)
 
 // View the current state of the canvas
 canvas
 
 // Draw the axes
 canvas.drawAxes()
+canvas.translate(byX: 0, byY: 300)
 
 // Add code below...
+let axiom = "F++F++F"
+let rule = "F-F++F-F"
+let angle = 60
+let length = 100
+
+func createWord(axiom: String, rule: String) -> String
+{
+    var newWord = ""
+    
+    for character in axiom.characters
+    {
+        (character == "F") ? (newWord += rule) : (newWord += String(character))
+    }
+    
+    return newWord
+}
+
+func drawWord(word: String)
+{
+    for character in word.characters
+    {
+        switch character {
+        case "F":
+            canvas.drawLine(fromX: 0, fromY: 0, toX: length, toY: 0)
+            canvas.translate(byX: length, byY: 0)
+            break
+        case "+":
+            canvas.rotate(by: (Degrees)(360 - angle))
+            break
+        case "-":
+            canvas.rotate(by: Degrees(angle))
+            break
+        default:
+            break
+        }
+    }
+}
+
+print(createWord(axiom: axiom, rule: rule)) // New word
+drawWord(word: createWord(axiom: axiom, rule: rule))
 
 
 /*:
